dda6a28ea6c54fdf1c13c59c3318571066cd039f
Isaac Luy <isaacluy@github.com>
2021-11-02 11:35:06 -0400

    Create a Block Turorial - step 3.2
    https://developer.wordpress.org/block-editor/getting-started/create-block/author-experience/

--- src/edit.js
+++ src/edit.js
@@ -1,24 +1,14 @@
 import { __ } from "@wordpress/i18n";
 import { useBlockProps } from "@wordpress/block-editor";
-import { Placeholder, TextControl } from "@wordpress/components";
+import { TextControl } from "@wordpress/components";
 import "./editor.scss";
 
-export default function Edit({ attributes, isSelected, setAttributes }) {
+export default function Edit({ attributes, setAttributes }) {
 	return (
-		<div {...useBlockProps()}>
-			{attributes.message && !isSelected ? (
-				<div>{attributes.message}</div>
-			) : (
-				<Placeholder
-					label={__("My First Block", "my-first-block")}
-					instructions={__("Add your message", "my-first-block")}
-				>
-					<TextControl
-						value={attributes.message}
-						onChange={(val) => setAttributes({ message: val })}
-					/>
-				</Placeholder>
-			)}
-		</div>
+		<TextControl
+			{...useBlockProps()}
+			value={attributes.message}
+			onChange={(val) => setAttributes({ message: val })}
+		/>
 	);
 }
